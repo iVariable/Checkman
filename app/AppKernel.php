3d0bdc4c114d4a91ffb45bf4c8632bafd65570d8
iVariable <ivariable@gmail.com>
2013-08-17 12:46:48 +0600

    bootstrap inited

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
-
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new CodeMonkeysRu\RepositoryAliasBundle\RepositoryAliasBundle(),
@@ -30,7 +28,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
--- src/Budget/ApplicationBundle/Controller/DefaultController.php
+++ src/Budget/ApplicationBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace Budget\ApplicationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+
+    /**
+     *
+     * @Route("/")
+     * @Template()
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
     {
-        return $this->render('ApplicationBundle:Default:index.html.twig', array('name' => $name));
+        return [];
     }
+
 }
