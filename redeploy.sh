604cd59139bf3cd8289ca0a6db18c9f1256633c8
iVariable <ivariable@gmail.com>
2014-05-02 21:43:06 +0600

    Fix redeploy for debian

--- redeploy.sh
+++ redeploy.sh
@@ -11,5 +11,6 @@ cd ..
 app/console assets:install --env=prod --symlink web -q
 app/console assetic:dump --env=prod web -q
 app/console cache:clear --env=prod -q
+#app/console budget:spendings:interval-fix `date -v-2m "+%d.%m.%Y"` -q
+app/console budget:spendings:interval-fix `date --date="-2 months" "+%d.%m.%Y"` -q
 chmod -R 0777 app/cache/ app/logs/
-app/console budget:spendings:interval-fix `date -v-2m "+%d.%m.%Y"` -q
